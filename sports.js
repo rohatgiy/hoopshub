9d58261c1b1f61013278ab26e6fae363db3ebb2e
Yash Rohatgi <rohatgiy@gmail.com>
2020-06-27 18:37:31 -0400

    Added some skeleton code

---
+++ sports.js
@@ -0,0 +1,7 @@
+var score1 = 0;
+var score2 = 0;
+var team1 = "a";
+var team2 = "b";
+
+console.log('the teams playing are ' + team1 + ' and ' + team2 + '.');
+console.log('score is ' + score1 + '-' + score2+ '.');

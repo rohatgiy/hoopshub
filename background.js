b4eb569cfa1a73851ba817857e4896709bc289c3
Yash Rohatgi <rohatgiy@gmail.com>
2020-07-12 21:30:05 -0400

    Final working version, only CSS styling left

--- background.js
+++ background.js
@@ -26,6 +26,6 @@ function createAlarm(today, date)
 function fireAlarm()
 {
     chrome.notifications.create('HoopsHub Game Reminder', {type: 'basic', 
-    iconUrl: './ball.png', title: 'HoopsHub Game Reminder', 
+    iconUrl: './hoopshub.png', title: 'HoopsHub Game Reminder', 
     message: 'Game in 1 hour!', contextMessage: 'Enjoy the game!'});
 }
